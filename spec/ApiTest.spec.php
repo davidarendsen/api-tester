151a6485f6eff469dc592acfee54a62b00cc4259
David Arendsen <darendsen@gamepoint.com>
2022-02-06 15:42:26 +0100

    Rename the spec name + added some more test cases to the schema

--- spec/ApiTest.spec.php
+++ spec/ApiTest.spec.php
@@ -4,20 +4,18 @@ use Arendsen\ApiTester\ApiTester;
 use Arendsen\ApiTester\SchemaSource;
 use Arendsen\ApiTester\SchemaSource\Type as SourceType;
 
-$options = [
-// 	'allowedRequestsToRun' => [
-// 		'GET /accesstokens'
-// 	],
-//	'disallowedRequestsToRun' => [
-//		'GET /users/{{userID}}',
-//	],
-];
-
 try {
 	$source = SchemaSource::create(SourceType::YAML);
 	$source->parseFile(__DIR__ . '/yaml_test.yaml');
 
-	$apiTester = new ApiTester($source, $options);
+	$apiTester = new ApiTester($source, [
+// 		'allowedRequestsToRun' => [
+// 			'GET /accesstokens'
+// 		],
+// 		'disallowedRequestsToRun' => [
+// 			'GET /users/{{userID}}',
+// 		],
+	]);
 	$apiTester->run();
 }
 catch(Exception $e) {
