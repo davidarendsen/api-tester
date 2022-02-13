273228ca6b112b816d21e47feaa2a2dc43eed4de
David Arendsen <darendsen@gamepoint.com>
2022-02-13 11:41:18 +0100

    Fix the tests

--- src/Matcher/Selector/Json.php
+++ src/Matcher/Selector/Json.php
@@ -2,6 +2,7 @@
 
 namespace Arendsen\ApiTester\Matcher\Selector;
 
+use Exception;
 use Arendsen\ApiTester\HttpResponse;
 
 class Json extends AbstractSelector {
@@ -10,6 +11,10 @@ class Json extends AbstractSelector {
 		$response = $this->getJsonDecodedResponse($httpResponse);
 
 		foreach($this->selection as $select) {
+			if(!isset($response[$select])) {
+				throw new Exception('Could not find array key (' . $select . ')');
+			}
+
             $response = $response[$select];
         }
 
