78e535acf7618c40c617adf33278b438be290bb6
David Arendsen <darendsen@gamepoint.com>
2022-02-13 11:55:12 +0100

    Add Selector test spec

--- spec/Matcher/Matcher.spec.php
+++ spec/Matcher/Matcher.spec.php
@@ -6,6 +6,7 @@ use Arendsen\ApiTester\SchemaSource\Type;
 use Arendsen\ApiTester\Schema\TestCase;
 use Arendsen\ApiTester\Matcher;
 use Arendsen\ApiTester\Matcher\Builder;
+use Arendsen\ApiTester\Matcher\Selector;
 
 describe('Matcher', function() {
 
@@ -35,6 +36,33 @@ describe('Matcher', function() {
 		it('is an instanceof Kahlan\Expectation', function() use ($testCase) {
 			$matcherBuilder = new Builder();
 			$matcherBuilder->setActualValue(200);
+
+			foreach($testCase->getMatchers() as $matcher => $matcherData) {
+				$matcherBuilder->addMatcher($matcher, $matcherData);
+			}
+
+			expect($matcherBuilder->match())->toBeAnInstanceOf('Kahlan\Expectation');
+		});
+	});
+
+	context('Matcher/Builder with Selector', function() use ($source) {
+		$testCase = new TestCase($source->toArray()[0]);
+
+		it('is an instanceof Kahlan\Expectation', function() use ($testCase) {
+			$httpClient = new HttpClient('https://reqres.in/api/');
+			$httpResponse = $httpClient->request(
+				'GET',
+				'users',
+				[]
+			);
+
+			$expectedValue = $testCase->getExpectedValue();
+
+			$selector = (new Selector())->create($expectedValue);
+			$selection = $selector->getSelection($httpResponse);
+
+			$matcherBuilder = new Builder();
+			$matcherBuilder->setActualValue($selection);
 
 			foreach($testCase->getMatchers() as $matcher => $matcherData) {
 				$matcherBuilder->addMatcher($matcher, $matcherData);
